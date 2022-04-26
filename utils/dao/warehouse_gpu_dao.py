e337d420f8074838962099e23e69375ce3b6988e
XOQDY <numfahmaek@gmail.com>
2022-04-27 01:18:03 +0700

    :hammer: add warehouse and gpu dao :hammer:

---
+++ utils/dao/warehouse_gpu_dao.py
@@ -0,0 +1,21 @@
+from models.warehouse_model import WarehouseModel
+from models.gpu_model import GPUModel
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+
+class WarehouseGPUDao:
+    def __init__(self, session: Session):
+        self.__session = session
+        self.__gpu = GPUModel
+        self.__warehouse = WarehouseModel
+
+    def get_all_gpu_by_zone(self, zone):
+        statement = select(self.__gpu, self.__warehouse).join("zone").filter_by(self.__warehouse == zone)
+        result = self.__session.execute(statement).all()
+        return result
+
+    def get_name_zone_by_zone(self, zone):
+        statement = select(self.__gpu.name, self.__warehouse).join("zone").filter_by(self.__warehouse == zone)
+        result = self.__session.execute(statement).all()
+        return result
