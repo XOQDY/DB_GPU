f80ea42dc149d9b2545a5fef4216b404ac6d12c5
XOQDY <numfahmaek@gmail.com>
2022-04-27 00:39:58 +0700

    :hammer: add Dao :hammer:

---
+++ utils/dao/gpu_dao.py
@@ -0,0 +1,34 @@
+from models.gpu_model import GPUModel
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+
+class GPUDao:
+    def __init__(self, session: Session):
+        self.__session = session
+        self.__gpu = GPUModel
+
+    def get_all(self):
+        statement = select(self.__gpu)
+        result = self.__session.execute(statement).all()
+        return result
+
+    def get_all_only_name(self):
+        statement = select(self.__gpu.name)
+        result = self.__session.execute(statement).all()
+        return result
+
+    def get_all_name_vendor_device(self):
+        statement = select(self.__gpu.name, self.__gpu.vendor, self.__gpu.device)
+        result = self.__session.execute(statement).all()
+        return result
+
+    def get_all_by_id(self, id):
+        statement = select(self.__gpu).filter_by(self.__gpu.id == id)
+        result = self.__session.execute(statement).all()
+        return result
+
+    def get_name_mobile_part_by_mobile_part(self, mobile_part):
+        statement = select(self.__gpu.name, self.__gpu.mobile_part).filter_by(self.__gpu.mobile_part == mobile_part)
+        result = self.__session.execute(statement).all()
+        return result
---
+++ utils/dao/warehouse_dao.py
@@ -0,0 +1,29 @@
+from models.warehouse_model import WarehouseModel
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+
+class WarehouseDao:
+    def __init__(self, session: Session):
+        self.__session = session
+        self.__warehouse = WarehouseModel
+
+    def get_all(self):
+        statement = select(self.__warehouse)
+        result = self.__session.execute(statement).all()
+        return result
+
+    def get_all_by_zone(self, zone):
+        statement = select(self.__warehouse).filter_by(self.__warehouse.zone == zone)
+        result = self.__session.execute(statement).all()
+        return result
+
+    def get_all_by_smaller_price(self, price):
+        statement = select(self.__warehouse).filter_by(self.__warehouse.price <= price)
+        result = self.__session.execute(statement).all()
+        return result
+
+    def get_all_have(self):
+        statement = select(self.__warehouse).filter_by(self.__warehouse.quantity > 0)
+        result = self.__session.execute(statement).all()
+        return result
